0813b7e3ea4f51c4b7e2a81315f66e2526926ba8
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2022-06-01 13:49:08 +0100

    Update IConjureRouter.sol

--- contracts/interfaces/IConjureRouter.sol
+++ contracts/interfaces/IConjureRouter.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-/// @author Conjure Finance Team
-/// @title IConjureRouter
-/// @notice Interface for interacting with the ConjureRouter Contract
-interface IConjureRouter {
+/// @author Scry Finance Team
+/// @title IScryRouter
+/// @notice Interface for interacting with the Router Contract
+interface IScryRouter {
 
     /**
      * @dev calls the deposit function
